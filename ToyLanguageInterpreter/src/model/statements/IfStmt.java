4b5afa1dc83accd00c5288ced51eec802c78a9c7
AleXutzZu <davidalexrobertyt@gmail.com>
2025-10-21 08:35:52 +0300

    Fix expression data type

--- ToyLanguageInterpreter/src/model/statements/IfStmt.java
+++ ToyLanguageInterpreter/src/model/statements/IfStmt.java
@@ -2,38 +2,39 @@ package model.statements;
 
 import exception.MyException;
 import model.PrgState;
+import model.expressions.IExp;
 import model.types.BoolType;
-import model.types.IType;
 import model.values.BoolValue;
 import model.values.IValue;
 
 public class IfStmt implements IStmt {
-    private Exp exp;
-    private  IStmt thenS;
+    private IExp exp;
+    private IStmt thenS;
     private IStmt elseS;
 
-    public IfStmt(Exp exp, IStmt thenS, IStmt elseS) {
-        this.exp=exp;
-        this.thenS=thenS;
-        this.elseS=elseS;
+    public IfStmt(IExp exp, IStmt thenS, IStmt elseS) {
+        this.exp = exp;
+        this.thenS = thenS;
+        this.elseS = elseS;
     }
-    public String toString(){
-        return "if("+exp+"){"+thenS+"}else{"+elseS+"}";
+
+    public String toString() {
+        return "if(" + exp + "){" + thenS + "}else{" + elseS + "}";
     }
-    public PrgState execute(PrgState state) throws MyException{
-       IValue val = exp.execute();
-       if(val.getType().equals(new BoolType())){
-           BoolValue b=(BoolValue)val;
-           if(b.getVal()==true){
-               state.getExeStack().push(thenS);
-           }
-           else
-           {
-               state.getExeStack().push(elseS);
-           }
-       }
-       else {
-           throw new MyException("The condition is not boolean type.");
-       }
+
+    public PrgState execute(PrgState state) throws MyException {
+        IValue val = exp.eval(state.getSymTable());
+        if (val.getType().equals(new BoolType())) {
+            BoolValue b = (BoolValue) val;
+            if (b.getVal() == true) {
+                state.getExeStack().push(thenS);
+            } else {
+                state.getExeStack().push(elseS);
+            }
+        } else {
+            throw new MyException("The condition is not boolean type.");
+        }
+
+        return state;
     }
 }
